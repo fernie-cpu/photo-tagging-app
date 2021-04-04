fbee6d77ecbe677fd9c0fcb01c4f86e57d2230ee
fernie-cpu <yoloisjustanick@gmail.com>
2021-04-04 12:57:10 -0300

    firebase config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-function App() {
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: 'waldo-app-d65d4.firebaseapp.com',
+  projectId: 'waldo-app-d65d4',
+  storageBucket: 'waldo-app-d65d4.appspot.com',
+  messagingSenderId: '38535726111',
+  appId: '1:38535726111:web:b2a4d256aa028d68fd812a',
+  measurementId: 'G-0DEN6XD1S7',
+};
+
+console.log(firebaseConfig.apiKey);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+const App = () => {
   return (
     <div className='App'>
       <header className='App-header'>
@@ -8,6 +25,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
