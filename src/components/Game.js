b5430eefb4efbf5a2c3cdeb33e6b5e9ed590ac8c
fernie-cpu <yoloisjustanick@gmail.com>
2021-04-05 03:48:50 -0300

    change img

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,11 +3,23 @@ import dbImg from '../img/dragon-ball.jpg';
 import Navbar from './Navbar';
 
 const Game = () => {
+  const handleClick = (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+    const coords = `X: ${x}, Y: ${y}`;
+    console.log(coords);
+  };
+
   return (
     <div>
       <Navbar />
       <div className='imgDiv'>
-        <img className='searchImg' src={dbImg} alt='dragon-ball' />
+        <img
+          className='searchImg'
+          src={dbImg}
+          alt='dragon-ball'
+          onClick={handleClick}
+        />
       </div>
     </div>
   );
